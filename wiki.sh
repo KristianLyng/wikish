efc35a1a4555d3a5637c34e67d855f55ca720014
Kristian Lyngstol <kristian@redpill-linpro.com>
2010-02-17 01:40:03 +0100

    Add DEBUG and clean up output a bit
    I'm particularly happy with the
    "{ GET | awk | sh ; if then err fi; } | grep" part.
    
    It's getting so weird it's almost perl.

--- wiki.sh
+++ wiki.sh
@@ -19,9 +19,49 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-# Order matters - 'cause I'm lazy.
+# Should probably >&2
+usage()
+{
+	cat <<_EOF_
+On a happier note:
 
+Configure stuff in wikish.config, then: 
+ ./wiki.sh GET Some_Page
+ ./wiki.sh POST Some_Page
+ ./wiki.sh EDIT Some_Page
+ ./wiki.sh CLEAN
+(Any trailing .wiki in the page-name is stripped)
 
+wiki.sh will (hopefully) not overwrite anything, but back it up for you.
+
+Configurations can be local (.wikish.config) or global (typically
+~/.config/wikish.config). See README for details.
+
+Oh yeah, and so far wikish only supports basic http auth
+... and no conflict-handling.
+_EOF_
+}
+
+debug() {
+	if [ -z "$DEBUG" ]; then
+		true;
+	else
+		echo $*
+	fi
+}
+
+################
+# "Global variables" that are not really affected by configuration
+################
+
+TIM=$(date +%s)
+ME=$(basename $0)
+ACTION=""
+PAGE=""
+
+################
+# Configuration
+################
 # Borrowed from compiz-manager: XDG base dirs is an ugly beast but I like
 # to do this just to remind people that you do not need a library to handle
 # this ugly mess. (I'm looking at you, awesome).
@@ -44,36 +84,16 @@ if [ -z "$API" ] || [ -z "$USER" ] || [ -z "$HOST" ] || \
 	[ -z "$PASSWORD" ] || [ -z "$PROTO" ]; then
 	echo "Insufficient or missing configuration."
 	echo "Copy $NAME to an xdg-path (typically ~/.config/, see \$XDG_CONFIG_DIRS)"
-	echo "(Hooray for XDG config dirs etc)"
 	exit 1;
 fi
 
 # Read local config
 test -f .wikish.config && . .wikish.config
 
-TIM=$(date +%s)
+################
+# Sym-link and argument mapping
+################
 
-# Should probably >&2
-usage()
-{
-	echo "RTFS!";
-	echo "On a happier note:"
-	echo " Configure stuff in wikish.config, then: "
-	echo " ./wiki.sh GET Some_Page"
-	echo " ./wiki.sh POST Some_Page"
-	echo " ./wiki.sh EDIT Some_Page"
-	echo " ./wiki.sh CLEAN (some random text because my argument-checking is lazy)"
-	echo " (Any trailing .wiki in the page-name is stripped)"
-	echo " Existing versions are not overwritten locally, but backed up."
-	echo " and are safe to remove."
-	echo " Oh yeah, and so far we/I/wikish only supports basich http auth"
-	echo " ... and no conflict-handling."
-}
-
-# Yeah, the placement is ugly. This script doesn't read nicely anymore.
-ME=$(basename $0)
-ACTION=""
-PAGE=""
 if [ $ME = "wikiedit" ]; then
 	ACTION="EDIT"
 	PAGE=$1
@@ -88,7 +108,8 @@ else
 	PAGE=$2
 fi
 
-if [ -z "$PAGE" ]; then
+
+if [ -z "$PAGE" ] && [ ! "x$1" = "xCLEAN" ]; then
 	usage;
 	exit 1;
 fi
@@ -96,11 +117,13 @@ fi
 # Allows for Main_Page and Main_Page.wiki - ie: tab completion.
 PAGE=$(echo $PAGE | sed s/.wiki$//);
 
+
 # Gets $PAGE and stores it to $PAGE.wiki
+# Safely backs up any existing $PAGE.wiki to $PAGE.wiki.$TIM
 getit()
 {
 	if [ -f "$PAGE.wiki" ]; then
-		echo "Moving $PAGE.wiki to $PAGE.wiki.$TIM"
+		debug "Moving $PAGE.wiki to $PAGE.wiki.$TIM"
 		if [ -f $PAGE.wiki.$TIM ]; then
 			echo "PANIC! already exist. Stop using loops!";
 			sleep 5;
@@ -111,7 +134,7 @@ getit()
 
 	GET "${PROTO}://${USER}:${PASSWORD}@${HOST}/index.php?title=${PAGE}&action=raw" > "$PAGE.wiki"
 	if [ $? = 0 ]; then
-		echo "$PAGE.wiki created seemingly without errors! Phew.";
+		debug "$PAGE.wiki created seemingly without errors! Phew.";
 	else
 		echo "$PAGE.wiki GET-operation may have blown apart.  Abandon ship!"
 		exit 2;
@@ -119,11 +142,14 @@ getit()
 }
 
 # Gets an edittoken and session for editing $PAGE and posts the local
-# $PAGE.wiki
+# $PAGE.wiki. Note that the awk-script generates a curl-command which is
+# piped to sh. Not terribly pretty.
 postit()
 {
  	BURL="${PROTO}://${USER}:${PASSWORD}@${HOST}/${API}"
- 	GET -USse "${BURL}action=query&format=txt&prop=info&intoken=edit&titles=$PAGE" | awk -v page="$PAGE" -v burl="$BURL" '
+	# Welcome to the school of funky shell-nesting.
+	{
+ 		GET -e "${BURL}action=query&format=txt&prop=info&intoken=edit&titles=$PAGE" | awk -v page="$PAGE" -v burl="$BURL" '
 		BEGIN {
 			wiki_session = "";
 			wikiToken = "";
@@ -147,14 +173,24 @@ postit()
 			gsub("+","%2B",edittoken);
 			title=page
 			gsub(" ","%20",title);
-			printf "echo | curl --post -k --data-urlencode \"text@"
+			printf "echo | curl -s --post -k --data-urlencode \"text@"
 			printf "%s.wiki\" ", page;
 			printf "-b wikiToken=" wikiToken " -b wiki_session=" wiki_session;
 			printf " \"" burl "format=txt&action=edit&title=%s&token=%s\" \n", title, edittoken
 		}
 		' | sh
+		if [ ! $? = "0" ]; then
+			echo "Failed to push. GET returned non-zero" >&2;
+			exit 1;
+		fi
+	
+	} | grep result
 }
 
+
+################
+# "Proper" execution starts here
+################
 
 if [ "x$ACTION" = "xGET" ]; then
 	getit
@@ -171,21 +207,20 @@ elif [ "x$ACTION" = "xEDIT" ]; then
 	else
 		$EDITOR "$PAGE.wiki"
 	fi
-	diff -q "$PAGE.wiki" "$PAGE.wiki.original.$TIM" && {
-		echo "No diff between original and new version... I think."
-		echo "(so not posting it)"
+	diff -q "$PAGE.wiki" "$PAGE.wiki.original.$TIM" >/dev/null && {
+		echo "Unchanged - not pushing it."
 		exit 1;
 	}
 	postit
 elif [ "x$ACTION" = "xCLEAN" ]; then
 	# Generate too much crap. One day I will put it in dot-files.
 	RMS=*.wiki.*[0-9]*
-	echo "About to kill: "
 	echo $RMS
-	echo "[Y]es/NOOOOOOOO!"
+	echo "Shall I kill the above files?"
+	echo "[Y]es/No!"
 	read yesno
-	if [ -z "$yesno" ] || [ "$yesno" = "Y" ] || [ "$yesno" = "y" ] || [ "$yesno" = "all work and no play makes jack a dull boy" ]; then
-		rm $RMS
+	if [ -z "$yesno" ] || [ "$yesno" = "Y" ] || [ "$yesno" = "y" ] || [ "x$yesno" = "xyes" ]; then
+		rm $RMS && echo "Done"
 	else
 		echo "Bailing!";
 	fi
